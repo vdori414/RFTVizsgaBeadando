b324697f0ba0d11b75779fb297e38656842c4dac
vdori414 <dora.vincze414@gmail.com>
2020-01-21 16:28:43 +0100

    Játék Form létrehozása

--- RFT_Jatek/RFT_Jatek/Form1.Designer.cs
+++ RFT_Jatek/RFT_Jatek/Form1.Designer.cs
@@ -89,7 +89,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Bejelentkezes";
+            this.Text = "Bejelentkezés";
             this.ResumeLayout(false);
             this.PerformLayout();
 
---
+++ RFT_Jatek/RFT_Jatek/JatekForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RFT_Jatek
+{
+    public partial class JatekForm : Form
+    {
+        public JatekForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
