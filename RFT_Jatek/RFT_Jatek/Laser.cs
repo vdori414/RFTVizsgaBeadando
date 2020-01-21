509ba8f5d60ea4a23c733abf09c36514f1936bab
vdori414 <dora.vincze414@gmail.com>
2020-01-21 18:08:50 +0100

    Laser osztály létrehozása

---
+++ RFT_Jatek/RFT_Jatek/Laser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFT_Jatek
+{
+    class Laser
+    {
+        private int sebzes;
+        public int Sebzes
+        {
+            get { return sebzes; }
+            set { sebzes = value; }
+        }
+
+        private Position position;
+        public Position Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        private int hossz;
+        public int Hossz
+        {
+            get { return hossz; }
+            set { hossz = value; }
+        }
+    }
+}
