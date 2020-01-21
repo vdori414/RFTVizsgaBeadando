596193f8fea51a74dc447f1c345a2a23bf744bb0
vdori414 <dora.vincze414@gmail.com>
2020-01-21 17:16:05 +0100

    Enemy osztály létrehozása

---
+++ RFT_Jatek/RFT_Jatek/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFT_Jatek
+{
+    class Enemy
+    {
+        private int elet;
+        public int Elet
+        {
+            get { return elet; }
+            set { elet = value; }
+        }
+    }
+}
