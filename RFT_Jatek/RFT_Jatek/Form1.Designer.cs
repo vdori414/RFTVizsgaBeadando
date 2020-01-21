e6032939b2ce0365bed98e2bf5545cb45a422386
vdori414 <dora.vincze414@gmail.com>
2020-01-21 15:57:35 +0100

    Bejelentkező felület kialakítása

--- RFT_Jatek/RFT_Jatek/Form1.Designer.cs
+++ RFT_Jatek/RFT_Jatek/Form1.Designer.cs
@@ -28,12 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_name = new System.Windows.Forms.TextBox();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(259, 46);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(455, 67);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Space Invaders";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(440, 219);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Név";
+            // 
+            // tb_name
+            // 
+            this.tb_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.tb_name.Location = new System.Drawing.Point(342, 252);
+            this.tb_name.Margin = new System.Windows.Forms.Padding(4);
+            this.tb_name.Name = "tb_name";
+            this.tb_name.Size = new System.Drawing.Size(261, 36);
+            this.tb_name.TabIndex = 2;
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btn_Start.Location = new System.Drawing.Point(415, 374);
+            this.btn_Start.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(108, 43);
+            this.btn_Start.TabIndex = 0;
+            this.btn_Start.Text = "Játék";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Navy;
+            this.ClientSize = new System.Drawing.Size(982, 653);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.tb_name);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Bejelentkezes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_name;
+        private System.Windows.Forms.Button btn_Start;
     }
 }
 
