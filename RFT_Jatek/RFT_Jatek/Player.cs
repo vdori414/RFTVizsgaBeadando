06f24fe77fb542ebf583e08520653dc67f86d4ab
bevi712 <basa.evelinn@gmail.com>
2020-01-21 17:25:18 +0100

    Karakterek feltöltése

---
+++ RFT_Jatek/RFT_Jatek/Player.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFT_Jatek
+{
+    class Player
+    {
+        private string nev;
+        public string Nev
+        {
+            get { return nev; }
+            set { nev = value; }
+        }
+
+        private int eletero;
+        public int Eletero
+        {
+            get { return eletero; }
+            private set
+            {
+                if (value < 0 || value > 100) throw new Exception("Hiba");
+                eletero = value;
+            }
+        }
+
+        private int sebzes;
+        public int Sebzes
+        {
+            get { return sebzes; }
+            set
+            {
+                if (value < 0) throw new Exception("Hiba");
+                sebzes = value;
+            }
+        }
+
+        private Position position;
+        public Position Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public void Sebzodik(int value)
+        {
+            if ((Eletero - value) < 0) Eletero = 0;
+            else Eletero -= value;
+        }
+
+        public void BalraMozgat(int value)
+        {
+            Position.Left -= value;
+        }
+
+        public void JobbraMozgat(int value)
+        {
+            Position.Left += value;
+        }
+
+        public Player(string Nev, int Eletero, int Sebzes)
+        {
+            this.Nev = Nev;
+            this.Eletero = Eletero;
+            this.Sebzes = Sebzes;
+            Position = new Position();
+        }
+
+        public Player(string Nev, int Eletero, int Sebzes,
+                        Position Position) : this(Nev, Eletero, Sebzes)
+        {
+            this.Position = Position;
+        }
+    }
+}
---
+++ RFT_Jatek/RFT_Jatek/Position.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFT_Jatek
+{
+    class Position
+    {
+        private int top;
+        public int Top
+        {
+            get { return top; }
+            set { top = value; }
+        }
+
+        private int left;
+        public int Left
+        {
+            get { return left; }
+            set { left = value; }
+        }
+
+        public Position()
+        {
+            this.Top = 0;
+            this.Left = 0;
+        }
+
+        public Position(int Top, int Left)
+        {
+            this.Top = Top;
+            this.Left = Left;
+        }
+    }
+}
